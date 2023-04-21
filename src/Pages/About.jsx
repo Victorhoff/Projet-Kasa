373d063717e34e677ad43b142986d0c596d2e283
victorhoff <victor.hoffmann@outlook.fr>
2023-04-21 15:05:50 +0200

    ajout du responsive

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import Banner from "../Components/Banner";
+import Collapse from "../Components/Collapse";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <main>
+      <Banner backgroundImg={"background.png"} />
+      <div className="collapse__about">
+        <div className="collapse__about__container">
+          <Collapse label="Fiabilité">
+            <p>
+              Les annonces postées sur Kasa garantissent une fiabilité totale.
+              Les photos sont conformes aux logements, et toutes les
+              informations sont régulièrement vérifiées par nos équipes.
+            </p>
+          </Collapse>
+        </div>
+        <div className="collapse__about__container">
+          <Collapse label="Respect">
+            <p>
+              La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+              comportement discriminatoire ou de perturbation du voisinage
+              entraînera une exclusion de notre plateforme.
+            </p>
+          </Collapse>
+        </div>
+        <div className="collapse__about__container">
+          <Collapse label="Service">
+            <p>
+              Nos équipes se tiennent à votre disposition pour vous fournir une
+              expérience parfaite. N'hésitez pas à nous contacter si vous avez
+              la moindre question.
+            </p>
+          </Collapse>
+        </div>
+        <div className="collapse__about__container">
+          <Collapse label="Responsabilité">
+            <p>
+              La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
+              pour les voyageurs, chaque logement correspond aux critères de
+              sécurité établis par nos services. En laissant une note aussi bien
+              à l'hôte qu'au locataire, cela permet à nos équipes de vérifier
+              que les standards sont bien respectés. Nous organisons également
+              des ateliers sur la sécurité domestique pour nos hôtes.
+            </p>
+          </Collapse>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default About;
